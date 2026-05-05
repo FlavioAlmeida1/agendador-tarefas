19101ed8e3bbbf9214c729992b0b2dbc196850e3
Flavio <flaviongo21@gmail.com>
2026-05-05 11:10:06 -0300

    adicionados metodos Get por email e por periodo

--- src/main/java/com/flavio/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/flavio/agendadortarefas/controller/TarefasController.java
@@ -3,8 +3,12 @@ package com.flavio.agendadortarefas.controller;
 import com.flavio.agendadortarefas.business.TarefasService;
 import com.flavio.agendadortarefas.business.dto.TarefasDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -18,5 +22,20 @@ public class TarefasController {
                                       @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(tarefasService.gravarTarefa(token , dto));
   }
+  @GetMapping("/eventos")
+    public ResponseEntity<List<TarefasDTO>> buscaDeTarefasPorPeriodo(
+          @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
+          @RequestParam @DateTimeFormat(iso= DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal){
+
+      return ResponseEntity.ok(tarefasService.buscaTarefasAgendadasPorPeriodo(dataInicial, dataFinal));
+
+  }
+  @GetMapping
+    public ResponseEntity<List<TarefasDTO>> buscaTarefasPorEmail(@RequestHeader("Authorization") String token){
+      List<TarefasDTO> tarefas = tarefasService.buscaTarefasPorEmail(token);
+
+      return ResponseEntity.ok(tarefas);
+  }
+
 
 }
